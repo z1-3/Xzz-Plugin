70495b0e030ebb91d890ba48e9ebec7222807690
j-3 <sayzo.dv@gmail.com>
2026-07-01 17:05:50 +0100

    update

--- src/main/java/com/yourname/plugin/OreSystem/OreListener.java
+++ src/main/java/com/yourname/plugin/OreSystem/OreListener.java
@@ -1,5 +1,7 @@
 package com.yourname.plugin.OreSystem;
 
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
@@ -17,33 +19,39 @@ public class OreListener implements Listener {
 
     @EventHandler
     public void onClick(InventoryClickEvent event){
+        
+        String title = event.getView().getTitle();
 
-        if(!(event.getView().getTitle().equals("OreMain"))){
-            return;
-        }
-
-        event.setCancelled(true);
-
-        if(event.getCurrentItem() == null){
-            return;
-        }
-
-        Material type = event.getCurrentItem().getType();
-
-        if(type == Material.DIAMOND_BLOCK){
+        if(title.equals("OreMain")){
+            event.setCancelled(true);
+            if(event.getCurrentItem() == null) return;
             Inventory Diamondgui = Bukkit.createInventory(null, 27, "Diamond Setting");
             Player player = (Player) event.getWhoClicked();
             ItemStack diachen = new ItemStack(Material.CLOCK);
-
+            
             ItemMeta meta = diachen.getItemMeta();
             meta.displayName(Component.text("Diamond Chance: " + DiamondChance));
+            meta.lore(List.of(
+                Component.text("Chance :" + DiamondChance + "% ")
+            ));
             diachen.setItemMeta(meta);
-
             Diamondgui.setItem(12, diachen);
-
             player.openInventory(Diamondgui);
 
         }
+
+        if(title.equals("Diamond Setting")){
+            event.setCancelled(true);
+            if(event.getCurrentItem() == null) return;
+        }
+
+    
+        
+        
+
+       
+
+       
 
 
     }
